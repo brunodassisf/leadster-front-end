8c580ff7327d50199df30941f6bf511241fa6db9
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-06-30 11:03:06 -0300

    create const for later use of media queries

---
+++ src/style/device.ts
@@ -0,0 +1,7 @@
+export const device = {
+    xs: "(max-width: 479px)",
+    sm: "(max-width: 767px)",
+    md: "(max-width: 991px)",
+    lg: "(max-width: 1199px)",
+    xl: "(min-width: 1200px)",
+};
