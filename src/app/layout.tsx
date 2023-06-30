a14a85e856e48ec37d0011919101fe5c6db120ce
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-06-30 10:51:15 -0300

    add fonts package and config for use

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { FontsPlusJakartaSans } from "../helper/localFont";
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="pt-BR">
+            <body className={FontsPlusJakartaSans.className}>{children}</body>
+        </html>
+    );
 }
---
+++ src/helper/localFont.ts
@@ -0,0 +1,76 @@
+import localFont from "next/font/local";
+
+export const FontsPlusJakartaSans = localFont({
+    src: [
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-ExtraLight.ttf",
+            weight: "200",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-ExtraLightItalic.ttf",
+            weight: "200",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-Light.ttf",
+            weight: "300",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-LightItalic.ttf",
+            weight: "300",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-Italic.ttf",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-Regular.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-Medium.ttf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-MediumItalic.ttf",
+            weight: "500",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-SemiBold.ttf",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-SemiBoldItalic.ttf",
+            weight: "600",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-BoldItalic.ttf",
+            weight: "700",
+            style: "italic",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-ExtraBold.ttf",
+            weight: "800",
+            style: "normal",
+        },
+        {
+            path: "../../public/static/font/PlusJakarta/PlusJakartaSans-ExtraBoldItalic.ttf",
+            weight: "800",
+            style: "italic",
+        },
+    ],
+});
