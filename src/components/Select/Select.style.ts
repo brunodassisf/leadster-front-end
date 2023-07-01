c4997811a4e28bfe1bbba2280d0913a4094c9aa1
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-06-30 22:49:24 -0300

    create select component

---
+++ src/components/Select/Select.style.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { device } from "@/src/style/device";
+import { theme } from "@/src/style/theme";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: fit-content;
+    align-items: center;
+    gap: ${theme.space[5]};
+    & label {
+        font-weight: ${theme.typography.weight[5]};
+    }
+    & .select_component__control {
+        border: 2px solid ${theme.color.grayDark};
+        border-radius: ${theme.space[6]};
+    }
+    & .select_component__indicator-separator {
+        display: none;
+    }
+    & .select_component__indicator {
+        color: ${theme.color.grayDark};
+    }
+    & .select_component__placeholder,
+    & .select_component__menu {
+        font-weight: ${theme.typography.weight[4]};
+        color: ${theme.color.grayDark};
+    }
+    & .select_container {
+        @media ${device.md} {
+            width: 100%;
+        }
+    }
+    @media ${device.md} {
+        flex-direction: column;
+        align-items: start;
+        width: 100%;
+    }
+`;
---
+++ src/components/Select/index.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React, { useId } from "react";
+import { FaCaretDown } from "react-icons/fa";
+import SelectCore, { GroupBase, Props, components } from "react-select";
+import { Container } from "./Select.style";
+
+interface ICustomProps {
+    label?: string;
+}
+
+interface IOption {
+    label: string;
+    value: string;
+}
+
+const CustomDropdownIndicator = (props) => {
+    return (
+        <components.DropdownIndicator {...props}>
+            <FaCaretDown size={22} />
+        </components.DropdownIndicator>
+    );
+};
+
+export default function Select<
+    IOption,
+    IsMulti extends boolean = false,
+    Group extends GroupBase<IOption> = GroupBase<IOption>
+>({
+    label,
+    id,
+    options,
+    ...props
+}: Props<IOption, IsMulti, Group> & ICustomProps) {
+    return (
+        <Container>
+            <label htmlFor={id}>{label}</label>
+            <SelectCore
+                className="select_container"
+                instanceId={id}
+                options={options}
+                classNamePrefix="select_component"
+                components={{ DropdownIndicator: CustomDropdownIndicator }}
+                {...props}
+            />
+        </Container>
+    );
+}
