593c9205606fb317405f00c28007eefd017d8fa2
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-03 11:31:58 -0300

    Creating styled Skeleton on the globalStyle

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,6 +1,11 @@
 import { theme } from "@/src/style/theme";
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
 import { device } from "./device";
+
+interface ISkeleton {
+    width: string;
+    height: string;
+}
 
 export const GlobalStyle = createGlobalStyle`
   html {
@@ -44,4 +49,44 @@ export const Container = styled.section`
     @media ${device.xl} {
         max-width: 1140px;
     }
+`;
+
+const skeletonAnimation = keyframes`
+  0% {
+    background-position: -200px 0;
+  }
+  100% {
+    background-position: 200px 0;
+  }
+`;
+
+const pulseAnimation = keyframes`
+  0% {
+    opacity: 0.8;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity: 0.8;
+  }
+`;
+
+const SkeletonWrapper = styled.div`
+    background: linear-gradient(
+        90deg,
+        rgba(240, 240, 240, 0.8) 10%,
+        rgba(224, 224, 224, 0.8) 30%,
+        rgba(240, 240, 240, 0.8) 90%
+    );
+    background-size: 200px 100%;
+    animation: ${skeletonAnimation} 3s infinite linear,
+        ${pulseAnimation} 1s infinite ease-in-out;
+    border-radius: ${theme.space[5]};
+`;
+
+export const Skeleton = styled(SkeletonWrapper)<ISkeleton>`
+    display: inline-block;
+    width: ${({ width }) => width};
+    height: ${({ height }) => height};
 `;
