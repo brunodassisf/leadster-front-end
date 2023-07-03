e7d4205eb98e693f466c0bc1a2943a4fe89b3a8f
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-03 12:24:34 -0300

    Import Demonstration component on the HomeScreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import WrapperPage from "@/src/components/WrapperPage";
 import Banner from "@/src/components/Banner";
 
 import Leadarea from "@/src/module/Leadarea";
+import Demonstration from "../components/Demonstration";
 
 export default function HomeScreen() {
     return (
         <WrapperPage>
             <Banner />
             <Leadarea />
+            <Demonstration />
         </WrapperPage>
     );
 }
