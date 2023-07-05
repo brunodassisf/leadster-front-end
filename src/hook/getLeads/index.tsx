3a0d1d79dc9d5c754ddec5a1ca6d67c92102920e
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-05 14:37:54 -0300

    Refactoring useGetLeads with type props for search

--- src/hook/getLeads/index.tsx
+++ src/hook/getLeads/index.tsx
@@ -4,11 +4,12 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 interface IuseGetLeadsProps {
     page: number;
+    type: number;
 }
 
-export default function useGetLeads<T>({ page }: IuseGetLeadsProps) {
+export default function useGetLeads<T>({ page, type }: IuseGetLeadsProps) {
     const { data, error, isLoading, mutate } = useSWR<T>(
-        `/api/leads?page=${page}`,
+        `/api/leads?page=${page}&type=${type}`,
         fetcher,
         {
             revalidateOnFocus: false,
