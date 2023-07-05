217e933c9d76b935418fb63e238781fa45d7b1cd
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-05 18:24:38 -0300

    Refactoring interface and mapping type

--- src/helper/interface/lead.ts
+++ src/helper/interface/lead.ts
@@ -8,11 +8,23 @@ export interface ILead {
     docFile: string;
     presensatioFile: string;
     videoUrl: string;
+    like: number;
 }
 export interface ILeads {
     leads: ILead[];
     totalPages: number;
 }
+
+export type ModalContentLead = Pick<
+    ILead,
+    | "name"
+    | "description"
+    | "thumbnail"
+    | "excelFile"
+    | "docFile"
+    | "presensatioFile"
+    | "videoUrl"
+>;
 
 type LeadContentModelProps = Pick<
     ILead,
