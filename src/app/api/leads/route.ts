48b45e31d59893d299d1e9e653dfe3a9ba47298c
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-05 18:28:53 -0300

    Refactoring the API route to use the filters too

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -1,3 +1,4 @@
+import { filterSwitch } from "@/src/helper/functionsCompare";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
@@ -5,16 +6,27 @@ export async function GET(request: Request) {
         const { searchParams } = new URL(request.url);
         const page = searchParams.get("page");
         const type = searchParams.get("type");
+        const filter = searchParams.get("filter");
         const data = await fetch("http://localhost:3000/api/acessData").then(
             (res) => res.json()
         );
 
         let getPageItems = {};
+        let arrWithFilter = [];
 
         if (Number(type) === 0) {
-            getPageItems = data.results[page ? Number(page) - 1 : 0];
+            if (filter !== "") {
+                for (let i = 0; data.results.length > i; i++) {
+                    for (let l = 0; data.results[i].leads.length > l; l++) {
+                        arrWithFilter.push(data.results[i].leads[l]);
+                    }
+                }
+                getPageItems = filterSwitch(filter, arrWithFilter);
+            } else {
+                getPageItems = data.results[page ? Number(page) - 1 : 0];
+            }
         } else {
-            let arr = [];
+            let arr: any[] = [];
             for (let i = 0; data.results.length > i; i++) {
                 for (let l = 0; data.results[i].leads.length > l; l++) {
                     if (data.results[i].leads[l].type === Number(type)) {
@@ -22,14 +34,16 @@ export async function GET(request: Request) {
                     }
                 }
             }
-            getPageItems = { leads: arr };
+            getPageItems = filterSwitch(filter, arr);
         }
 
         return NextResponse.json({
             ...getPageItems,
-            totalPages: Number(type) === 0 ? data.results.length : 0,
+            totalPages:
+                Number(type) === 0 && filter === "" ? data.results.length : 0,
         });
     } catch (error) {
+        console.log(error);
         return NextResponse.json({
             error: true,
             msg: "Ocorreu um erro ao buscar os cursos",
