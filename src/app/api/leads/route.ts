4a0a7c99dd73a203cb164a301ee559d196e32dc8
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-05 14:41:19 -0300

    Refactoring API handler lead refactoring to use type when passing querie params

--- src/app/api/leads/route.ts
+++ src/app/api/leads/route.ts
@@ -4,15 +4,30 @@ export async function GET(request: Request) {
     try {
         const { searchParams } = new URL(request.url);
         const page = searchParams.get("page");
+        const type = searchParams.get("type");
         const data = await fetch("http://localhost:3000/api/acessData").then(
             (res) => res.json()
         );
 
-        const getPageItems = data.results[page ? Number(page) - 1 : 0];
+        let getPageItems = {};
+
+        if (Number(type) === 0) {
+            getPageItems = data.results[page ? Number(page) - 1 : 0];
+        } else {
+            let arr = [];
+            for (let i = 0; data.results.length > i; i++) {
+                for (let l = 0; data.results[i].leads.length > l; l++) {
+                    if (data.results[i].leads[l].type === Number(type)) {
+                        arr.push(data.results[i].leads[l]);
+                    }
+                }
+            }
+            getPageItems = { leads: arr };
+        }
 
         return NextResponse.json({
             ...getPageItems,
-            totalPages: data.results.length,
+            totalPages: Number(type) === 0 ? data.results.length : 0,
         });
     } catch (error) {
         return NextResponse.json({
