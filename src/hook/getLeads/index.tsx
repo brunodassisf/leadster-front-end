918670600b87f697bbe173cca73332c7355460a2
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-05 18:26:32 -0300

    Refactoring hooke useGetLeads for use filter

--- src/hook/getLeads/index.tsx
+++ src/hook/getLeads/index.tsx
@@ -5,11 +5,16 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 interface IuseGetLeadsProps {
     page: number;
     type: number;
+    filter: string;
 }
 
-export default function useGetLeads<T>({ page, type }: IuseGetLeadsProps) {
+export default function useGetLeads<T>({
+    page,
+    type,
+    filter,
+}: IuseGetLeadsProps) {
     const { data, error, isLoading, mutate } = useSWR<T>(
-        `/api/leads?page=${page}&type=${type}`,
+        `/api/leads?page=${page}&type=${type}&filter=${filter}`,
         fetcher,
         {
             revalidateOnFocus: false,
