0c002371959b89b7248eb352323d5991e2d393b5
Bruno de Assis Ferreira <33126607+brunodassisf@users.noreply.github.com>
2023-07-06 10:59:08 -0300

    Adding metadata keywords, url and themeColor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-import Head from "next/head";
 import React from "react";
+import { Metadata } from "next";
+import Head from "next/head";
 
 import StyledComponentsRegistry from "../lib/StyledComponentsRegistry";
 import AOSRegistry from "../lib/AOSRegistry";
@@ -7,13 +8,21 @@ import AOSRegistry from "../lib/AOSRegistry";
 import { FontsPlusJakartaSans } from "../helper/localFont";
 import { openGraphImage } from "./shared-metadata";
 
-export const metadata = {
+export const metadata: Metadata = {
     title: "Leadster",
+    keywords: [
+        "Leadster",
+        "Mais Conversão",
+        "estratégias",
+        "negócio",
+        "Geração de Leads",
+    ],
+    themeColor: "white",
     description: "Transformando visitantes em clientes",
     openGraph: {
         title: "Leadster",
         description: "Transformando visitantes em clientes",
-        url: "",
+        url: "https://leadster-front-end-ashen.vercel.app/",
         siteName: "Leadster",
         locale: "pt_BR",
         type: "website",
